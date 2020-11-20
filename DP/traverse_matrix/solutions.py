739f26955285d936ac3758925d5bd78faca45907
jaewoo.kim <chrysaor@naver.com>
2020-11-20 10:02:48 +0900

    Add a method of the computation table.

--- DP/traverse_matrix/solutions.py
+++ DP/traverse_matrix/solutions.py
@@ -4,11 +4,33 @@ class Solution:
         self.matrix = []
 
     def init_mat(self, rows, cols):
-        for row in range(rows):
-            self.matrix.append([0 for _ in range(cols)])
+        """Initiation solution matrix
+        :type rows: int
+        :type cols: int
+        """
+        self.matrix = [[0] * cols for _ in range(rows)]
 
-    def find_path(self, rows: int, cols: int, x: int, y: int) -> None:
-        """Find a path with recursive O(2^n)"""
+    def find_path_dp(self, rows: int, cols: int) -> None:
+        """Find a path with a table - O(M * N)"""
+        # SETTING TRIVIAL UNIQUE PATH
+        for row in range(rows):
+            self.matrix[row][0] = 1
+
+        for col in range(cols):
+            self.matrix[0][col] = 1
+
+        # FILL A MATRIX WITH DP TABLE -> CURRENT WAY = UP WAY + LEFT WAY
+        for row in range(1, rows):
+            for col in range(1, cols):
+                self.matrix[row][col] = self.matrix[row][col-1] + self.matrix[row-1][col]
+
+    def find_path_graph(self, rows: int, cols: int, x: int, y: int) -> None:
+        """Find a path with recursive O(|V| + |E|)
+        :type rows: int
+        :type cols: int
+        :type x: int
+        :type y: int
+        """
         if x >= rows or y >= cols:
             return
 
@@ -17,11 +39,11 @@ class Solution:
 
         # MOVE RIGHT
         if -1 < x < cols and -1 < y < rows:
-            self.find_path(rows, cols, x, y+1)
+            self.find_path_graph(rows, cols, x, y+1)
 
         # MOVE DOWN
         if -1 < x < cols and -1 < y < rows:
-            self.find_path(rows, cols, x+1, y)
+            self.find_path_graph(rows, cols, x+1, y)
 
     def unique_paths(self, rows: int, cols: int) -> int:
         """Find unique path
@@ -30,7 +52,7 @@ class Solution:
         :rtype: int
         """
         self.init_mat(rows, cols)
-        self.find_path(rows, cols, 0, 0)
+        self.find_path_dp(rows, cols)
 
         for row in range(rows):
             print(self.matrix[row])
@@ -38,4 +60,5 @@ class Solution:
         return self.matrix[rows-1][cols-1]
 
 
-print(Solution().unique_paths(3, 3))
+if __name__ == '__main__':
+    print(Solution().unique_paths(5, 5))
