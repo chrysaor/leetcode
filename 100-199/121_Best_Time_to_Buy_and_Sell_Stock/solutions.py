442605e5a9e5da95b9b1a530899dc6eecbd1ff53
jaewoo.kim <chrysaor@naver.com>
2020-11-23 12:54:55 +0900

    Add a DP solution to sell stock problem

--- 100-199/121_Best_Time_to_Buy_and_Sell_Stock/solutions.py
+++ 100-199/121_Best_Time_to_Buy_and_Sell_Stock/solutions.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """Calculate max profit with the dynamic programming"""
+        """Calculate max profit with the min max calculation"""
+        if prices is None or len(prices) <= 1:
+            return 0
+
         max_idx = 0
         max_res = 0
 
@@ -17,7 +20,22 @@ class Solution:
 
         return max(max_res, 0)
 
+    def maxProfitDp(self, prices: List[int]) -> int:
+        """Calculate max profit with the dynamic programming"""
+        if prices is None or len(prices) <= 1:
+            return 0
 
-print(Solution().maxProfit([3, 2, 6, 5, 0, 3]))
-print(Solution().maxProfit([12, 4, 6, 9, 3, 8, 4]))
-print(Solution().maxProfit([6, 5, 4, 3, 2, 1]))
+        optimal = [0] * len(prices)
+
+        # O(N): Loop for all prices
+        # OPT[IDX] = max(0, OPT[IDX-1] + TODAY_EARNING)
+        for idx in range(1, len(prices)):
+            today_earn = prices[idx] - prices[idx-1]
+            optimal[idx] = max(0, optimal[idx-1] + today_earn)
+
+        return max(optimal)
+
+
+print(Solution().maxProfitDp([2, 1]))
+print(Solution().maxProfitDp([12, 4, 6, 9, 3, 8, 4]))
+print(Solution().maxProfitDp([6, 5, 4, 3, 2, 1]))
