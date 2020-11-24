45f4425892823cca8ab6d85d65b76ed9fd5c0d2c
jaewoo.kim <chrysaor@naver.com>
2020-11-24 13:21:45 +0900

    Add solutions for a problem of number 238

--- 100-199/121_Best_Time_to_Buy_and_Sell_Stock/README.md
+++ 100-199/121_Best_Time_to_Buy_and_Sell_Stock/README.md
@@ -9,6 +9,7 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.```
+```
 
 ```
 Example 1:
@@ -28,3 +29,4 @@ Output: 0
 
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 ```
+
---
+++ 200-299/238_Product_of_Array_Except_Self/solutions.py
@@ -0,0 +1,82 @@
+from functools import reduce
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """Product array of dynamic way - O(N) because its count in loop 3N"""
+        left_sum, right_sum = [], []
+
+        temp = 1
+        for num in nums:
+            temp = num*temp
+            left_sum.append(temp)
+
+        temp = 1
+        for num in nums[::-1]:
+            temp = num*temp
+            right_sum.append(temp)
+
+        right_sum = right_sum[::-1]
+
+        result = []
+        for idx in range(1, len(nums)-1):
+            left_val = left_sum[idx-1]
+            right_val = right_sum[idx+1]
+            result.append(left_val*right_val)
+
+        return [right_sum[1]] + result + [left_sum[len(nums)-2]]
+
+    def product_except_self_short(self, nums: List[int]) -> List[int]:
+        """Product array of dynamic way - O(N) because its count in loop 2N"""
+        out = []
+
+        p = 1
+        # left product
+        for i in range(0, len(nums)):
+            out.append(p)
+            p *= nums[i]
+
+        p = 1
+        # right product
+        for i in range(len(nums) - 1, 0 - 1, -1):
+            out[i] *= p
+            p = p * nums[i]
+
+        return out
+
+    def product_except_self_brute_force(self, nums: List[int]) -> List[int]:
+        """Product array of brute force way - O(N^2) because its count in loop 3N product list size N"""
+        return [reduce((lambda x, y: x * y), nums[:idx] + nums[idx+1:]) for idx in range(len(nums))]
+
+    def productExceptCurrentElement(self, arr):
+        '''
+        :type arr: list of int
+        :rtype: list of int
+        '''
+        left_sum, right_sum = [], []
+
+        temp = 1
+        for num in arr:
+            temp = num*temp
+            left_sum.append(temp)
+
+        temp = 1
+        for num in arr[::-1]:
+            temp = num*temp
+            right_sum.append(temp)
+
+        right_sum = right_sum[::-1]
+
+        result = []
+        for idx in range(1, len(arr)-1):
+            left_val = left_sum[idx-1]
+            right_val = right_sum[idx+1]
+            result.append(left_val*right_val)
+
+        return [right_sum[1]] + result + [left_sum[len(arr)-2]]
+
+
+if __name__ == '__main__':
+    print(Solution().productExceptCurrentElement([1, 2, 3]))
+    print(Solution().productExceptSelf([12, 4, 6, 8]))
