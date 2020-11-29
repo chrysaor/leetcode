4aef6e75c18a2f6d308e89aee653f68a0409cf95
keanu <chrysaor@naver.com>
2020-11-30 00:41:37 +0900

    Update some solutions for the problem 39

--- 1-100/39_Combination_Sum/solutions.py
+++ 1-100/39_Combination_Sum/solutions.py
@@ -1,20 +1,19 @@
+import collections
 from typing import List
 
 
 class Solution:
     def __init__(self):
         self.dp = {}
-        self.current = {}
         self.ans_list = []
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        used_list = [0] * len(candidates)
-        self.my_reach_score(target, candidates, used_list)
-        return self.ans_list
+        # used_list = [0] * len(candidates)
+        return self.another_score_dp(candidates, target)
 
     def my_reach_score(self, score: int, point_list: List[int], used_list: List[int]):
         """Find combination sum with recursive approach"""
-        key = ','.join(str(item) for item in used_list)
+        key = ''.join(str(item) for item in used_list)
 
         if key in self.dp or score < 0:
             return 0
@@ -35,17 +34,38 @@ class Solution:
             ans += self.my_reach_score(score - point, point_list, used_list)
             used_list[idx] -= 1
 
-        self.dp[key] = ans
-
         return ans
 
     def my_reach_score_updated(self, candidates, target, curr_nums):
         """Find combination sum with recursive approach"""
-        pass
+        if target == 0:
+            return [curr_nums]
 
-    def my_reach_score_dp(self, candidates, target):
-        """Find combination sum with recursive approach"""
-        pass
+        if len(candidates) == 0 or target <= 0:
+            return []
+
+        # Get current candidates
+        c = candidates[0]
+        sol = self.my_reach_score_updated(candidates[1:], target, curr_nums)
+        sol.extend(self.my_reach_score_updated(candidates, target - c, curr_nums + [c]))
+
+        return sol
+
+    def another_score_dp(self, candidates, target):
+        """Find combination sum with dynamic programming approach"""
+        dp = collections.defaultdict(list)
+        dp[0] = [[]]
+
+        candidates.sort()
+        # Item in [2, 3, 7]
+        for c in candidates:
+            # Candidate for a target number
+            for t in range(c, target + 1):
+                # Because of the default dict, if dp[t-c] doesn't exist, make new default list []
+                for comb_t_m_c in dp[t-c]:
+                    dp[t].append(comb_t_m_c + [c])
+
+        return dp[target]
 
 
-print(Solution().combinationSum([2, 3, 7], 12))
+print(Solution().combinationSum([2, 3, 7], 122))
